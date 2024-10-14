4f26a1a490e839e7299a98ed993f6ed2c011fef8
Enrikas <ramasauskasenrikas@gmail.com>
2024-10-14 13:46:31 +0300

    created customnav component

--- scripts/components.js
+++ scripts/components.js
@@ -1,3 +1,80 @@
+function customNavbar() {
+  return `    
+  <nav>
+    <div class="widthContainer">
+        <h1>Logo</h1>
+
+        <ul>
+        <li class="active"><a href="/">Pagrindinis</a></li>
+        <li><a href="/store">Parduotuvė</a></li>
+        </ul>
+
+        <div class="actionBtnContainer">
+        <button>
+            <a href="/"><i class="fa-solid fa-cart-shopping"></i></a>
+        </button>
+        <button>
+            <a href="sign-up"><i class="fa-regular fa-user"></i></a>
+        </button>
+
+        <button class="mobileMenu">
+            <a href="/"><i class="fa-solid fa-bars"></i></a>
+        </button>
+        </div>
+
+        <div class="miniBasket">
+        <h1>Mano krepšelis</h1>
+
+        <div class="basketList">
+            <div class="basketItem">
+            <div class="imageContainer">
+                <img src="/assets/images/pelyte.png" width="90" />
+            </div>
+
+            <article>
+                <h1>Logitech pelytė</h1>
+                <h3>49.99€</h3>
+
+                <div class="countContainer">
+                <button><i class="fa-solid fa-square-plus"></i></button>
+                <input type="number" value="1" min="1" />
+                <button><i class="fa-solid fa-square-minus"></i></button>
+                </div>
+
+                <button class="removeItem">
+                <i class="fa-solid fa-trash-can"></i>
+                </button>
+            </article>
+            </div>
+        </div>
+
+        <div class="totalContainer">
+            <div class="total">
+            <h1>Išviso:</h1>
+            <span>49444.99€</span>
+            </div>
+        </div>
+
+        <div class="buttonContainer">
+            <a class="basketBtn" href="/basket">Krepšelis</a>
+            <a class="checkOutBtn" href="/checkout">Apmokėjimas</a>
+        </div>
+        </div>
+    </div>
+</nav>
+    `;
+}
+
+function fillNavbars() {
+  let navbars = document.querySelectorAll("CustomNav");
+
+  for (const navbar of navbars) {
+    navbar.innerHTML = customNavbar();
+  }
+}
+
+fillNavbars();
+
 function customFooter() {
   return `
        <footer>
